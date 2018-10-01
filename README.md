a06179281f9924064f223dc15621a0cb4020659c
M <michael.bekaert@stir.ac.uk>
2018-10-01 15:46:46 +0100

    Deploy "Coming back soon"

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
-
 # Mile of Limes
 
 > Welcome to Mile of Limes. I am Margaret M. O' Toole, creative writer. I spend my time dreaming up worlds, with characters and stories that keep me wanting to see what they will do next.
@@ -9,17 +7,17 @@ Back-end of [mileofLimes.org](https://mileoflimes.org). Basic page and deploymen
 
 ### History
 
-2018-09-03 - Transfer to Heroku/Github. Now using PHP app (free dyno) via Cloudflare.
+2018-10-01 - Transfer to Heroku/Github. Now using PHP app (free dyno) via [Cloudflare](https://www.cloudflare.com/).
 
 2018-09-03 - **Temporary closure**. 
 
 2017-04-01 - Deployment using Dockers only.
 
-2016-03-01 - Transfer to AracCloud via Cloudflare.
+2016-03-26 - Transfer to [Aruba Cloud](https://www.arubacloud.com/) via [Cloudflare](https://www.cloudflare.com/).
 
-2015-03-03 - **Grand opening**.
+2015-04-12 - **Grand opening**.
 
-2015-03-01 - Deployment of Apache + Wordpress on Amazon EC2 (free tier) via Cloudflare.
+2015-03-28 - Deployment of Apache + Wordpress on [Amazon AWS](http://aws.amazon.com/) (free tier) via [Cloudflare](https://www.cloudflare.com/).
 
 ### License
 
--- web/index.php
+++ web/index.php
@@ -1,4 +1,58 @@
-<?php
-header("Location: https://mileoflimes.org", true, 301);
-exit();
-?>
+<!DOCTYPE html>
+<html lang="en-GB">
+  <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <title>Mile of Limes | Leaves of imagination</title>
+    <link rel="canonical" href="https://mileoflimes.org/">
+    <meta name="keywords" content="Margaret O'Toole, Mile of Limes, Creative Writing">
+    <meta name="description" content="Mile of Limes - Leaves of imagination">
+
+    <!-- Schema.org -->
+    <meta itemprop="name" content="Mile of Limes | Leaves of imagination">
+    <meta itemprop="description" content="Mile of Limes - Leaves of imagination by Margaret O'Toole">
+    <meta itemprop="image" content="https://mileoflimes.org/mileoflimes_thumbnail.jpg">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@creativetim">
+    <meta name="twitter:title" content="Mile of Limes | Leaves of imagination">
+    <meta name="twitter:description" content="Mile of Limes - Leaves of imagination by Margaret O'Toole">
+    <meta name="twitter:creator" content="@creativetim">
+    <meta name="twitter:image" content="https://mileoflimes.org/mileoflimes_thumbnail.jpg">
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Mile of Limes | Leaves of imagination">
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="https://mileoflimes.org/">
+    <meta property="og:image" content="https://mileoflimes.org/mileoflimes_thumbnail.jpg">
+    <meta property="og:description" content="Mile of Limes - Leaves of imagination by Margaret O'Toole">
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link href="mileoflimes.css" rel="stylesheet" type="text/css" media="all">
+    <link href="https://fonts.googleapis.com/css?family=Grand+Hotel" rel="stylesheet" type="text/css" media="all"> 
+    <link href="https://use.fontawesome.com/releases/v5.3.1/css/regular.css" rel="stylesheet" type="text/css" media="all" integrity="sha384-ZlNfXjxAqKFWCwMwQFGhmMh3i89dWDnaFU2/VZg9CvsMGA7hXHQsPIqS+JIAmgEq" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" rel="stylesheet" type="text/css" media="all" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="oe.js"></script>
+  </head>
+  <body>
+    <div class="main">
+      <div class="cover"></div>
+        <div class="container">
+          <h1 class="title">Mile of Limes</h1>
+          <div class="content">
+            <h2>Leaves of imagination</h2>
+            <p>(I am busy re-training, but I will be back very soon)</p>
+          </div>
+       </div>
+    </div>
+    <div class="footer">
+      <div class="container">
+        <i class="far fa-envelope"></i>&nbsp;<a class="zulu" href="#" rel="nofollow" data-value="481 173 350 105 127 105 119 233 5 105 127 381 37 410 127 105 5 381 358 219 37 95 51">#</a> 
+      </div>
+    </div>
+  </body>
+</html>
